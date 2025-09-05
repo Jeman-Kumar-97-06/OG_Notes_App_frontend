e59ed4936ea95c30108b02207fed34f558c3af17
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-09-05 14:23:20 +0530

    Added ThemeContext.jsx

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import { createContext,useReducer } from "react";
 
 export const ThemeContext = createContext();
 
 export const themeReducer = (state,action) => {
     switch(action.type){
+        case 'DO_DARK':
+            return {
+                theme : "dark"
+            }
+        case "DO_LIGHT":
+            return {
+                theme : "light"
+            }
         default:
             return state;
     }
@@ -11,6 +20,15 @@ export const themeReducer = (state,action) => {
 
 export const ThemeContextProvider = ({children}) => {
     const [state,dispatch] = useReducer(themeReducer,{theme:"light"});
+    useEffect(()=>{
+        const thme = JSON.parse(localStorage.getItem('theme'));
+        if (thme == 'dark'){
+            dispatch({type:'DO_DARK'})
+        }
+        if (thme == 'light') {
+            dispatch({type:"DO_LIGHT"})
+        }
+    },[])
     return (
         <ThemeContext.Provider value={{...state,dispatch}}>
             {children}
