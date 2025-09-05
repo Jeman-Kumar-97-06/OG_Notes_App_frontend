8c5cc2531f00c5a763111291ed2f0d79ad72f07e
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-09-05 07:34:43 +0530

    Added ThemeContext.jsx

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -4,6 +4,16 @@ export const ThemeContext = createContext();
 
 export const themeReducer = (state,action) => {
     switch(action.type){
-        
+        default:
+            return state;
     }
-} 
+};
+
+export const ThemeContextProvider = ({children}) => {
+    const [state,dispatch] = useReducer(themeReducer,{theme:"light"});
+    return (
+        <ThemeContext.Provider value={{...state,dispatch}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
