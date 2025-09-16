14ad9ce7318ff749e8a5751dda5288a21151a6a5
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-09-16 17:56:17 +0530

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Home from "./components/HomePage"
 import AuthDialog from "./components/Login_Signup"
 import JournalHome from "./components/MainPage"
-import MinimalistEcom from "./components/MinEcom"
+import MinimalistEcom from "./components/MinEcom";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
     return (
     <>
-      <Home/>
-      <AuthDialog/>
-      <JournalHome/>
-      <MinimalistEcom/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/main' element={<JournalHome/>}/>
+          <Route exact path='/auth' element={<AuthDialog/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
